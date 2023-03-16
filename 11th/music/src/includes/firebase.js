3752055e0f025c4ffa4499b2c3662636e15dd00c
Palak Zalavadia <palak.z@simformsolutions.com>
2023-03-16 17:23:07 +0530

    Authentication in Vue

---
+++ 11th/music/src/includes/firebase.js
@@ -0,0 +1,28 @@
+// import firbase module
+//import specific module called app from firebase
+// app module is the core of firebase SDK
+import firebase from "firebase/app"
+import "firebase/auth"
+import 'firebase/firestore'  //not import 'firebase/database'  database is for old verion of databse whereas firestore is new database , database is for real time database  
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgEccqvESiTfpgOzmIEdTVyJgLF8hsahw",  //How we will be able to connect to firebase(to inform firebase we are the one sending request to firebase not someone else)
+    authDomain: "music-75597.firebaseapp.com", //firebase provides URL where we can send authentication info. It is needed if we want to add aunthentication to our app 
+    projectId: "music-75597",
+    storageBucket: "music-75597.appspot.com",  //bucket is used to describe the location where the files are stored
+    appId: "1:706513215087:web:873f5eedf469c7cb0cf79a"  //firebase allows multiple apps to be connected to our project
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');  //db.collection has one argument which is the name of the collection to select
+
+export{
+    auth,db,
+    usersCollection,
+};
+
+//In this file we have integrated firebase SDK in our project
