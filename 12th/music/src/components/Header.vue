e9734731a424eb4c114889b57ac37c908062107b
Palak Zalavadia <palak.z@simformsolutions.com>
2023-03-17 14:23:39 +0530

    Routing

--- 12th/music/src/components/Header.vue
+++ 12th/music/src/components/Header.vue
@@ -3,11 +3,14 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name:'home'}" exact-active-class="no-active">Music</router-link> 
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link :to="{name : 'about'}" class="px-2 text-white">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -16,10 +19,10 @@
           </li>
           <template v-else>
             <li >
-            <a class="px-2 text-white" href="#">Manage</a>
+            <router-link :to="{name : 'manage'}" class="px-2 text-white" href="#">Manage</router-link>
           </li>
           <li >
-            <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+            <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
           </li>
           </template>
          
@@ -40,6 +43,14 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen //idnameStore
 
+    },
+    signOut(){
+      this.userStore.signOut()
+
+      if(this.$route.meta.requiresAuth){
+        this.$router.push({name:"home"})
+      }
+     
     }
   },
   computed: {
