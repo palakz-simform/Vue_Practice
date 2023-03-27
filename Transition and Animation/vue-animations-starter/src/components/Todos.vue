991664f814932d5915616b04b2d114699fef7bde
Palak Zalavadia <palak.z@simformsolutions.com>
2023-03-27 13:32:47 +0530

    Video 8 Transitioning between elements (between if-else block)

--- Transition and Animation/vue-animations-starter/src/components/Todos.vue
+++ Transition and Animation/vue-animations-starter/src/components/Todos.vue
@@ -6,7 +6,8 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
       <transition-group tag="ul" name="list" appear>
         <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
           {{ todo.text }}
@@ -14,6 +15,8 @@
       </transition-group>
     </div>
     <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
+
   </div>
 </template>
 
@@ -106,4 +109,17 @@ export default {
   .list-move{
     transition: all 0.3s ease;
   }
+  /* switch transition */
+  .switch-enter-from,.switch-leave-to{
+    opacity:0;
+    transform: translateY(20px)
+  }
+  /* .switch-enter-to,.switch-leave-from{
+    opacity:1;
+    transform: translateY(0);
+  } */
+  .switch-enter-active, .switch-leave-active{
+    transition: all 0.5s ease;
+  }
+
 </style>
