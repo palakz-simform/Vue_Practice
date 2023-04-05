6f356fa88259def623fd1489861e4a1a77592009
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-05 14:41:19 +0530

    Video 208 Global Directive

--- music/src/main.js
+++ music/src/main.js
@@ -1,28 +1,29 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import {auth} from "./includes/firebase"
+import { auth } from "./includes/firebase"
 
 import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
-
+import Icon from "./directives/icon"
 
 import "./assets/base.css";
 import "./assets/main.css";
 
 let app;
 
-auth.onAuthStateChanged(()=>{   //method provided by firebase Authentication that is used to listen for changes in the user authenticate state
-  
-    if(!app){
+auth.onAuthStateChanged(() => {   //method provided by firebase Authentication that is used to listen for changes in the user authenticate state
+
+    if (!app) {
         app = createApp(App);
         app.use(createPinia());
         app.use(router);
         app.use(VeeValidatePlugin);
-        
+        app.directive("icon", Icon)
+
         app.mount("#app");
     }
-    
-    
+
+
 })
 
