9d8c37052adb04a49ad8042680378d57f7c76c24
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-10 14:52:01 +0530

    Video 229

--- music/src/main.js
+++ music/src/main.js
@@ -1,37 +1,35 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { auth } from "./includes/firebase"
 
 import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
-import Icon from "./directives/icon"
-import i18n from "./includes/i18n"
-import { registerSW } from "virtual:pwa-register"
-import GlobalComponents from "./includes/_globals"
+import { auth } from "./includes/firebase";
+import Icon from "./directives/icon";
+import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
+import GlobalComponents from "./includes/_globals";
 
 import "./assets/base.css";
 import "./assets/main.css";
 
-registerSW({ immediate: true });
+
+// registerSW({ immediate: true });
+
 
 let app;
 
-auth.onAuthStateChanged(() => {   //method provided by firebase Authentication that is used to listen for changes in the user authenticate state
-
+auth.onAuthStateChanged(() => {  //method provided by firebase authentication
     if (!app) {
         app = createApp(App);
+
         app.use(createPinia());
         app.use(router);
         app.use(VeeValidatePlugin);
-        app.use(i18n)
+        app.use(i18n);
         app.use(GlobalComponents);
-        app.use("icon", Icon)
-        app.directive("icon", Icon)
+        app.directive("icon", Icon);
 
         app.mount("#app");
     }
-
-
-})
-
+});
--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -99,16 +99,19 @@ export default {
       });
     }
   },
-  async created() {
-    const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: 'home' });
-      return;
-    }
-    const { sort } = this.$route.query;
-    this.sort = sort === '1' || sort === '2' ? sort : '1';
-    this.song = docSnapshot.data();
-    this.getComments();
+  async beforRouteEnter(to, from, next) {
+    const docSnapshot = await songsCollection.doc(to.params.id).get();
+    next((vm) => {
+      if (!docSnapshot.exists) {
+        vm.$router.push({ name: 'home' });
+        return;
+      }
+      const { sort } = thvmis.$route.query;
+      vm.sort = sort === '1' || sort === '2' ? sort : '1';
+      vm.song = docSnapshot.data();
+      vm.getComments();
+    });
+
   },
   methods: {
     ...mapActions(usePlayerStore, ["newSong"]),
