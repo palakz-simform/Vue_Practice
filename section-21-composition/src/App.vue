f0a6c9c64184cafaa13e1e2b3b6fd47b8f488d52
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-17 16:44:43 +0530

    Video 255

--- section-21-composition/src/App.vue
+++ section-21-composition/src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }}</p>
+    <button @click.prevent="increment">Click me</button>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: "App",
+  setup() {
+    let num = ref(0);
+    // console.log(num)
+    function increment() {
+      num.value++;
+    }
+
+    return {
+      num, increment
+    }
+  }
 };
 </script>
