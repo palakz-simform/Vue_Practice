111a8409a54581cd623e970c145cc79b764b39d5
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-20 11:29:55 +0530

    Section 18

---
+++ music/src/router/index.js
@@ -0,0 +1,77 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+import useUserStore from "@/stores/user";
+
+
+// creating chuncks.
+const Home = () => import("@/views/Home.vue");
+const About = () => import("@/views/About.vue");
+const Manage = () => import("@/views/Manage.vue");
+const Song = () => import("@/views/Song.vue");
+
+const routes = [
+  {
+    name: 'home',
+    path: "/",
+    component: Home,
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About,
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',  // can be considered as additional path
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log("Manage Route Guard");
+      next();
+    },
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+});
+
+//beforeEach function is available after instance of the router is created
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta);
+
+  if (!to.meta.requiresAuth) {
+    next();
+    return
+  }
+  const store = useUserStore();
+  if (store.userLoggedIn) {
+    next();
+  }
+  else {
+    next({ name: "home" })
+  }
+});
+
+export default router; //router is registered in the vue instance
