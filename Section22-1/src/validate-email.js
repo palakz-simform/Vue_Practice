9ffd6468f0814a2fa3573f1f97795bb9262c697f
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-25 11:01:19 +0530

    Imported from Section 19

--- Section22-1/src/validate-email.js
+++
@@ -1,3 +0,0 @@
-export default function validateEmial(email) {
-    return email.length >= 4 && email.length > 0
-}
---
+++ music/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <app-header></app-header>
+
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+
+  <AppPlayer />
+
+  <auth></auth>
+</template>
+
+<script>
+import AppHeader from '@/components/Header.vue'
+import Auth from '@/components/Auth.vue'
+import { mapWritableState } from 'pinia';
+import useUserStore from '@/stores/user';
+import { auth } from './includes/firebase'
+import AppPlayer from "@/components/Player.vue"
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    Auth,
+    AppPlayer
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+  //this life cycle method is called when the function is created 
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.1s linear;
+  opacity: 0;
+}
+</style>
