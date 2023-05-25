d303775f2b604ca78d310d6b0c3297b68518fbbb
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-25 10:55:23 +0530

    sECTION 22

---
+++ Section22-1/src/components/EmailInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <input type="email" class="form-control" placeholder="E-mail" :value="modelValue" @input="update" :class="{
+            'is-valid': validateEmail(modelValue),
+
+            'is-invalid':
+                !validateEmail(modelValue),
+        }" />
+</template>
+
+<script>
+import validateEmail from '../validate-email';
+export default {
+    name: 'EmailInput',
+    props: ['modelValue'],
+    // modelValue should be default name
+    methods: {
+        update($event) {
+            this.$emit('update:modelValue', $event.target.value);
+        },
+        validateEmail,
+    }
+}
+</script>
+
+<style scoped></style>
